f2e9d38343bb5b47c6750d23e71f0e4472802a86
Jorge Cáncer Gil <jorgecancerps@gmail.com>
2017-03-14 16:04:20 +0100

    First commit

---
+++ src/main/java/com/ucode/HackathonHandlerApplication.java
@@ -0,0 +1,12 @@
+package com.ucode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonHandlerApplication.class, args);
+	}
+}
